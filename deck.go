f69f6abd9947165211e9be0f6d0f124e863eaed6
Prajakta Sawant <prajakta.sawant@thoughtworks.com>
2021-06-15 15:48:55 +0530

    Story #1 : Create the deck

---
+++ deck.go
@@ -0,0 +1,17 @@
+package main
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+
+	return cards
+}
