a67250f0267a920b65529d78129a808247a069c8
Prajakta Sawant <prajakta.sawant@thoughtworks.com>
2021-06-17 19:19:06 +0530

    Story #11 : Delete from Map and Iterate Map

--- map/main.go
+++ map/main.go
@@ -3,10 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string {
+	colors := map[string]string{
 		"red": "#ff0000",
 		"green": "#00ff00",
+		"white": "#ffffff",
 	}
+	print(colors)
 
-	fmt.Println(colors)
+	// var sample map[string]string
+	sample := make(map[string]string)
+	sample["white"] = "#ffffff"
+	delete(sample, "white")
+	fmt.Println(sample)
+}
+
+func print(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
